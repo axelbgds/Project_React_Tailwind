cab25af374d6e04775dbb654a4f425123a6dc8b3
Axel Beaugrand <77735666+axelbgds@users.noreply.github.com>
2022-08-17 16:50:55 +0200

    Change function to use Effect

--- src/App.js
+++ src/App.js
@@ -1,61 +1,34 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageList from "./components/ImageList";
 import ImageSearch from "./components/ImageSearch";
 import "./assets/main.css";
 
-function APIData(props) {
+function APIData() {
   const [images, setImages] = useState([]); // image list
-  const [isLoading, setIsLoading] = useState(false);
-  const [imageSearch, setImageSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [imageSearch, setImageSearch] = useState('');
 
-  const handleImageSearchSubmit = useCallback(
-    async (e) => {
-      e.preventDefault();
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          `https://pixabay.com/api/?key=29223106-80cc5ba29708d378849977f12&q=${imageSearch}&image_type=photo`
-        );
-        if (response.ok) {
-          const { hits } = await response.json();
-          setImages(hits);
-        } else {
-          throw new Error(
-            "There is a problem to fetch data with this API (the key is probably wrong)"
-          );
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://pixabay.com/api/?key=29223106-80cc5ba29708d378849977f12&q=${imageSearch}&image_type=photo`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
         setIsLoading(false);
-      }
-    },
-    [imageSearch]
-  );
-
-  const noImageMessage = useMemo(
-    () =>
-      !isLoading && images && images.length === 0 ? (
-        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
-      ) : null,
-    [isLoading, images]
-  );
-
-  const loadingMessage = useMemo(
-    () => (isLoading ? <h1 className="text-5xl text-center mx-auto mt-32">Loading...</h1> : null),
-    [isLoading]
-  );
+      }).catch(err => console.log(err));
+  }, [imageSearch]);
 
   return (
-    <div>
-      <ImageSearch
-        setImageSearch={setImageSearch}
-        handleImageSearchSubmit={handleImageSearchSubmit}
-      />
-      {noImageMessage}
-      {loadingMessage}
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setImageSearch(text)} />
 
-      <ImageList images={images} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageList key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
